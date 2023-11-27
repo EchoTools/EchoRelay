0ad2734dd2c00f19c0d6124e9548de5a3464d4bc
NotBlue <enzodubocage@gmail.com>
2023-11-27 21:53:43 +0100

    public endpoint

---
+++ EchoRelay.API/Controllers/Public/PublicServersController.cs
@@ -0,0 +1,36 @@
+﻿using EchoRelay.Core.Game;
+using EchoRelay.Core.Server.Services.ServerDB;
+using EchoRelay.Core.Server.Storage;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using EchoRelay.Core.Server;
+
+namespace EchoRelay.API.Controllers
+{
+    [Route("serversList/")]
+    [ApiController]
+    public class PublicServersController : ControllerBase
+    {
+        static Server? ServiceServer => ApiServer.Instance?.RelayServer.ServerDBService.Server;
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                if (ServiceServer == null)
+                {
+                    return StatusCode((int)HttpStatusCode.InternalServerError, "Server is null");
+                }
+
+                var serverInfo = new PublicServerInfo(ServiceServer);
+                return Ok(serverInfo);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ EchoRelay.API/Controllers/Public/PublicSessionsController.cs
@@ -0,0 +1,49 @@
+﻿using EchoRelay.Core.Game;
+using EchoRelay.Core.Server.Services.ServerDB;
+using EchoRelay.Core.Server.Storage;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using EchoRelay.Core.Server.Messages.ServerDB;
+
+namespace EchoRelay.API.Controllers
+{
+    [Route("sessionsList/")]
+    [ApiController]
+    public class PublicSessionsController : ControllerBase
+    {
+        static GameServerRegistry? Registry => ApiServer.Instance?.RelayServer.ServerDBService.Registry;
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                if (Registry == null)
+                {
+                    return StatusCode((int)HttpStatusCode.InternalServerError, "Registry is null");
+                }
+
+                var publicSessions = new List<PublicSessionInfo>();
+                var servers = Registry.RegisteredGameServersBySessionId.Keys;
+                foreach (var sessionId in servers)
+                {
+                    var gameServer = Registry.GetGameServer(sessionId);
+                    if (gameServer != null)
+                    {
+                        if(gameServer.SessionLobbyType != ERGameServerStartSession.LobbyType.Private)
+                        {
+                            publicSessions.Add(new PublicSessionInfo(gameServer));
+                        }
+                    }
+                }
+
+                return Ok(publicSessions);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
